01ae8bbc704b8e1ecbd1f71108b13cf097baf5ec
Nycky10 <jeanboh8@gmail.com>
2018-08-08 21:26:36 +0000

    premier commit

---
+++ src/main/java/com/telco/SMOsprinter/SmOsprinterApplication.java
@@ -0,0 +1,12 @@
+package com.telco.SMOsprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmOsprinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmOsprinterApplication.class, args);
+	}
+}
